fb4dd6c3c3eecdcfb848e7edb8003a52aebd1c7b
Thai Khac Nguyen <shiroemon279@gmail.com>
2018-10-03 17:30:26 +0700

    Sửa lại một số hàm cho từ điển

--- VietlishDictionary/src/vietlishdictionary/VietlishDictionary.java
+++ VietlishDictionary/src/vietlishdictionary/VietlishDictionary.java
@@ -1,6 +1,5 @@
 package vietlishdictionary;
 
-import java.util.*;
 import dictionary.backend.*;
 
 public class VietlishDictionary {
@@ -11,13 +10,6 @@ public class VietlishDictionary {
         DictionaryCommandLine tuDien = new DictionaryCommandLine();
         
         tuDien.dictionaryAdvanced();
-        
-        /* Code để test dictionarySearch()
-        ArrayList<Word> test = tuDien.dictionarySearch();
-        for (int i = 0; i < test.size(); i++)
-        	System.out.println(test.get(i).getWord_target());
-       	*/
-        
     }
     
 }
